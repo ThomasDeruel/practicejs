c8dd3782c8723f7d25dccc517e93682eae7ddf67
Thomas <t.deruel@hotmail.fr>
2018-01-14 15:24:12 +0100

    pierre feuille ciseaux fonctionnel

--- exercice2-pierre-feuille-ciseau/script.js
+++ exercice2-pierre-feuille-ciseau/script.js
@@ -2,69 +2,60 @@ var cards = document.querySelectorAll('.card');
 var value="";
 var result=document.querySelectorAll('.result');
 var flipped = document.querySelectorAll('.cardItem');
-var score= {
-  player:0,
-  ia:0
-};
+var win =0;
+var loose =0;
 var bg ={pierre:"<img class='img'src='pierre.png' alt='pierre' >",
-  feuille:"<img class='img'src='feuille.png' alt='feuille' >",
-  ciseaux:"<img class='img'src='ciseaux.png' alt='ciseaux' >"
-}
-var text = {
-  win:"<h2 class='result'>Gagné</h2>",
-  loose:"<h2 class='result'>Perdu</h2>"
-}
-
-/*var aleatoire = function(value)
-{
-    var rand = Math.floor(Math.random()*100);
-  if(rand <33 ){ value = "pierre";}
-  else if(rand >33 && rand <66){ value = "Feuille";}
-  else { value="ciseaux";}
-
-
-}*/
+        feuille:"<img class='img'src='feuille.png' alt='feuille' >",
+        ciseaux:"<img class='img'src='ciseaux.png' alt='ciseaux' >"}
+var text = {win:"<h2 class='result'>Gagné</h2>",
+           loose:"<h2 class='result'>Perdu</h2>"}
+var pierre = document.querySelector('.pierre');
+var feuille = document.querySelector('.feuille');
+var ciseaux = document.querySelector('.ciseaux');
 
 for (let i= 0; i < cards.length; i++) {
+
   cards[i].addEventListener('click',function(){
-
-    var card = cards[i];
     flipped[i].classList.add('flipped');
-
-    var rand = Math.floor(Math.random()*100);
-    if(rand <33 ){value = "pierre";}
-    else if(rand >33 && rand <66){value = "feuille";}
+    var rand = Math.floor(Math.random()*3)+1;
+    if(rand ===1 ){value = "pierre";}
+    else if(rand ===2){value = "feuille";}
     else {value="ciseaux";}
-
-    console.log("ordi :"+value);
-    console.log("vous :"+card.dataset.type);
-    if (cards[0]){// pierre
-    if(value === card.dataset.type)
-    { result[i].innerHTML = "Egalité";}
-    else if(value == 'feuille'){result[i].innerHTML = text.loose + bg.feuille; return score.ia++;}
-    else{result[i].innerHTML = text.win+bg.ciseaux; return score.player++;}
-    console.log(score.player);
-    }
-    if (cards[1]){//feuille
-    if(value === card.dataset.type)
-    { result[i].innerHTML = "Egalité";}
-    else if(value == 'ciseaux'){result[i].innerHTML = text.loose+bg.ciseaux ;return score.ia++;}
-    else{result[i].innerHTbg.pierre;return  score.player++;}
-    console.log(score.player);
-    }
-    if (cards[2]){//ciseaux
-    if(value === card.dataset.type)
-    { result[i].innerHTML = "Egalité";}
-    else if(value == 'pierre'){result[i].innerHTML = text.loose+bg.pierre ;return score.ia++;}
-    else{result[i].innerHTML = text.win+bg.feuille;return score.player++;}
-    console.log(score.player);
-    }
-
-  })
-  flipped[i].addEventListener("transitionend", function(e) {
-  document.querySelector('.player').innerHTML = "Vous: " + score.player+"/5";
-  document.querySelector('.ia').innerHTML = "IA: " +score.ia+"/5" ;
-
-  flipped[i].classList.remove('flipped');
   });
+  }
+  pierre.addEventListener('click',function(){
+    if(value == pierre.dataset.type)
+    { result[0].innerHTML = "Egalité";}
+    else if(value == 'feuille'){result[0].innerHTML = text.loose + bg.feuille;loose++;}
+    else{result[0].innerHTML = text.win+bg.ciseaux;win++;}
+  });
+
+  feuille.addEventListener('click',function(){
+    if(value == feuille.dataset.type)
+    { result[1].innerHTML = "Egalité";}
+    else if(value == 'ciseaux'){result[1].innerHTML = text.loose + bg.ciseaux;loose++;}
+    else{result[1].innerHTML = text.win+bg.pierre;win++;}
+  });
+  ciseaux.addEventListener('click',function(){
+
+    if(value == ciseaux.dataset.type)
+    { result[2].innerHTML = "Egalité";}
+    else if(value == 'pierre'){result[2].innerHTML = text.loose + bg.pierre;loose++;}
+    else{result[2].innerHTML = text.win+bg.feuille; win++;}
+  });
+
+
+
+for (let j = 0; j < flipped.length; j++) {
+  flipped[j].addEventListener("transitionend", function() {
+  flipped[j].classList.remove('flipped');
+  if(win<5 || loose<5)
+  {
+  document.querySelector('.player').innerHTML = "Vous: " + win+"/5";
+  document.querySelector('.ia').innerHTML = "IA: " +loose+"/5" ;
+  }
+  else{  document.querySelector('.player').innerHTML = "Vous: 5/5";
+    document.querySelector('.ia').innerHTML = "IA: 5/5" ;}
+
+})
 }
